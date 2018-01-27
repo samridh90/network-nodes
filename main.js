1c23fb886b1cb5f03a55fe9105f3afc5bb498017
Samridh Srinath <samridh90@gmail.com>
2018-01-26 20:10:53 -0800

    Part a: Trivially show all the nodes and their edges

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-  let count = 50000;
+  let count = 5000;
   let seed = 1234567;
   nodes = generateNodes(count, seed);
   renderTable(nodes);
@@ -10,7 +10,7 @@ function renderTable(nodes) {
   let $table = $('<table></table>');
   let $thead = $('<thead></thead>');
   let $tbody = $('<tbody></tbody>');
-  let $header = $('<tr><th>Node</th><th>Edges</th></tr>');
+  let $header = $('<tr><th class="node-col">Node</th><th>Edges</th></tr>');
   $container.append(
     $table
       .append($thead.append($header))
@@ -20,5 +20,9 @@ function renderTable(nodes) {
 }
 
 function renderNodes($tbody, nodes) {
-
+  let rows = _.map(_.toPairs(nodes), ([key, value]) => {
+      let edges = _.keys(value).join(' ');
+      return $(`<tr><td>${key}</td><td>${edges}</td></tr>`);
+    });
+  $tbody.append(rows);
 }
